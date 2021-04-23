f3435f59ea132fbd2637f9862be04c043f6ec267
Anthony Townsend <anthony@starcitygorup.us>
2021-04-23 18:28:09 -0400

    fastapi hello world working

--- api.py
+++ api.py
@@ -3,6 +3,10 @@ from datetime import date, datetime, timedelta
 from dateutil import parser
 
 
+from fastapi import FastAPI
+import uvicorn
+
+
 from flask import Flask, render_template, request, jsonify, abort, send_from_directory
 from flask_cors import CORS
 from flask_restful import Resource, Api
@@ -14,7 +18,7 @@ import Database as db
 
 from dotenv import load_dotenv
 load_dotenv()
-api_key = os.getenv("MAPBOX_API_KEY")
+
 api_url_stem="/api/v1/nyc/livemap"
 
 from config import config
@@ -32,9 +36,12 @@ from config import config
 db_connect = create_engine(db.get_db_url(config.config['dbuser'], config.config['dbpassword'], config.config[
     'dbhost'], config.config['dbport'], config.config['dbname']))
 # to 'localhost' for debugging?
-app = Flask(__name__,template_folder='./api/templates',static_url_path='/static',static_folder="api-www/static/")
-api = Api(app)
-CORS(app)
+
+app = FastAPI()
+
+#app = Flask(__name__,template_folder='./api/templates',static_url_path='/static',static_folder="api-www/static/")
+#api = Api(app)
+#CORS(app)
 
 
 #--------------- HELPER FUNCTIONS ---------------
@@ -114,81 +121,63 @@ def results_to_FeatureCollection(results):
         geojson['features'].append(feature)
     return geojson
 
-
-#--------------- API ---------------
-
-class LiveMap(Resource):
-    def get(self):
-        import geojson
-        with open('./api/static/lastknownpositions.geojson', 'r') as infile:
-            return geojson.load(infile)
-
-class RouteQuerySchema(Schema):
-    route_short = fields.Str(required=True)
-    start = fields.Str(required=True)  # in ISO 8601 e.g. 2020-08-11T14:42:00+00:00
-    end = fields.Str(required=True)  # in ISO 8601 e.g. 2020-08-11T15:12:00+00:00
-    output = fields.Str(required=True)
-
-route_schema = RouteQuerySchema()
-
-class SystemAPI(Resource):
-    def get(self):
-        errors = route_schema.validate(request.args)
-        if errors:
-            abort(400, str(errors))
-        conn = db_connect.connect()
-        query_prefix = "SELECT * FROM buses WHERE {}"
-        query_suffix = query_builder(request.args)
-        query_compound = query_prefix.format(query_suffix )
-        print (query_compound)
-        query = conn.execute(query_compound)
-        results = {'observations': unpack_query_results(query)}
-
-        if request.args['output'] == 'geojson':
-            print(str(len(results['observations'])) + ' positions returned via API')
-            return results_to_FeatureCollection(results)
-        else: #only geojson for now
-            return {'API error':'incorrect output format type. only "geojson" supported at the time'}
+#-------------- Fast API -------------------------------------------------------------
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-#--- ENDPOINTS ---#
+# #--------------- Old API ---------------
+#
+# class LiveMap(Resource):
+#     def get(self):
+#         import geojson
+#         with open('./api/static/lastknownpositions.geojson', 'r') as infile:
+#             return geojson.load(infile)
+#
+# class RouteQuerySchema(Schema):
+#     route_short = fields.Str(required=True)
+#     start = fields.Str(required=True)  # in ISO 8601 e.g. 2020-08-11T14:42:00+00:00
+#     end = fields.Str(required=True)  # in ISO 8601 e.g. 2020-08-11T15:12:00+00:00
+#     output = fields.Str(required=True)
+#
+# route_schema = RouteQuerySchema()
+#
+# class SystemAPI(Resource):
+#     def get(self):
+#         errors = route_schema.validate(request.args)
+#         if errors:
+#             abort(400, str(errors))
+#         conn = db_connect.connect()
+#         query_prefix = "SELECT * FROM buses WHERE {}"
+#         query_suffix = query_builder(request.args)
+#         query_compound = query_prefix.format(query_suffix )
+#         print (query_compound)
+#         query = conn.execute(query_compound)
+#         results = {'observations': unpack_query_results(query)}
+#
+#         if request.args['output'] == 'geojson':
+#             print(str(len(results['observations'])) + ' positions returned via API')
+#             return results_to_FeatureCollection(results)
+#         else: #only geojson for now
+#             return {'API error':'incorrect output format type. only "geojson" supported at the time'}
+#
+#
+# #--- ENDPOINTS ---#
+#
+# #----for map----#
+# api.add_resource(LiveMap, '/api/v1/nyc/livemap')
+#
+# #----for apps----#
+# # /api/v1/nyc/buses?output=geojson&route_short=Bx4&start=2021-03-28T00:00:00+00:00&end=2021-03-28T01:00:00+00:00
+# # gives out all positions on a given 'route_short' during the specific interval in ISO 8601 format
+# # from 'start'
+# # to 'end'
+# # max interval = 1 hour
+# # output=geojson for now
+# api.add_resource(SystemAPI, '/api/v1/nyc/buses', endpoint='buses')
 
-#----for map----#
-api.add_resource(LiveMap, '/api/v1/nyc/livemap')
 
-#----for apps----#
-# /api/v1/nyc/buses?output=geojson&route_short=Bx4&start=2021-03-28T00:00:00+00:00&end=2021-03-28T01:00:00+00:00
-# gives out all positions on a given 'route_short' during the specific interval in ISO 8601 format
-# from 'start'
-# to 'end'
-# max interval = 1 hour
-# output=geojson for now
-api.add_resource(SystemAPI, '/api/v1/nyc/buses', endpoint='buses')
-
-
-#-----------------------------------------------------------------------------------------
-
-
-
-# is the browser s# @app.route('/')
-# # def index():
-# #     return render_template('index.html')
-# #
-# # @app.route('/map')
-# # def map():
-# #     return render_template('map.html')
-# #
-# # @app.route('/occupancy')
-# # def occupancy():
-# #     return render_template('occupancy.html')
-# #
-# # @app.route('/faq')
-# # def faq():
-# #     return render_template('faq.html')till caching this — wrap this in an http header to expire it?
-# @app.route('/api/v1/nyc/lastknownpositions')
-# def lkp():
-#     print (app.static_folder)
-#     return send_from_directory(app.static_folder,'lastknownpositions.geojson')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    uvicorn.run(app,debug=True)
