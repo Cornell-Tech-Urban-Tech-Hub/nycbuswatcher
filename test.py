f43b54ea67264c8f377aeccf8a75bb624f17e5b6
Anthony Townsend <anthony@starcitygorup.us>
2021-06-04 21:00:07 -0400

    checkpoint

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ runtime_args = parser.parse_args()
 
 lake = dump.DataLake(runtime_args)
 print('rendering {} puddles'.format( len(lake.puddles) ) )
-print([str(p.folder.path) for p in lake.puddles])
+# rint([str(p.folder.path) for p in lake.puddles])
 
-store = dump.DataStore(runtime_args)
-print('rendering {} barrels'.format( len(store.barrels) ) )
-print([str(b.folder.path) for b in store.barrels])
+# store = dump.DataStore(runtime_args)
+# print('rendering {} barrels'.format( len(store.barrels) ) )
+# # print([str(b.folder.path) for b in store.barrels])
