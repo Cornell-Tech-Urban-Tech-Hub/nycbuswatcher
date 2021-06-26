0dfd21178876ef8bc715897f6c4cc28a5863fe67
Anthony Townsend <amt3@nyu.edu>
2021-06-25 21:43:09 -0400

    added validation and constraints for all endpoints

--- api.py
+++ api.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 from os.path import isfile
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Query, Path
 from typing import Optional
 from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
-from pathlib import Path
+import pathlib
 
 from common.Models import DatePointer, DateRoutePointer, DataStore, Shipment
 
@@ -53,7 +53,7 @@ class PrettyJSONResponse(Response):
 
 def make_store(): #bug too costly? how to automate this for refresh periodically
     print ('i recreated the DataStore()')
-    return DataStore(Path.cwd())
+    return DataStore(pathlib.Path.cwd())
 
 
 #------------------------------------------------------------------------------------------------------------------------
@@ -64,70 +64,37 @@ async def discover_endpoints(request: Request):
     # return templates.TemplateResponse("index.html", {"request": request})
     return RedirectResponse("/docs")
 
-#------------------------------------------------------------------------------------------------------------------------
-# ENDPOINT /api/v2/nyc/dashboard
-# FUNCTION Dashboard metadata showing number of barrels and shipments per hour per route currently stored.
-@app.get('/api/v2/nyc/dashboard')
-# after https://stackoverflow.com/questions/62455652/how-to-serve-static-files-in-fastapi
-async def fetch_dashboard_data():
-    filename= 'data/dashboard.csv'
-    if not isfile(filename):
-        return Response(status_code=404)
-    with open(filename) as f:
-        content = f.read()
-        return Response(content, media_type='text/csv')
-
 
 #------------------------------------------------------------------------------------------------------------------------
-# ENDPOINT /api/v2/nyc/buses/{year}/{month}/{day}/{hour}/{route}/json
-# FUNCTION get a single Shipment by date_pointer as JSON
-@app.get('/api/v2/nyc/{year}/{month}/{day}/{hour}/{route}/buses')
-# after https://stackoverflow.com/questions/62455652/how-to-serve-static-files-in-fastapi
-async def fetch_single_shipment(year,month,day,hour,route):
-    shipment_to_get = 'data/store/shipments/{}/{}/{}/{}/{}/shipment_{}-{}-{}-{}-{}.json'.\
-        format(year,month,day,hour,route.upper(),year,month,day,hour,route.upper())
-    if not isfile(shipment_to_get):
-        return Response(status_code=404)
-    with open(shipment_to_get) as f:
-        content = f.read()
-    return Response(content, media_type='application/json')
-
-
-#------------------------------------------------------------------------------------------------------------------------
-# ENDPOINT /api/v2/nyc/buses/{year}/{month}/{day}/{hour}/{route}/geojson
-# FUNCTION get a single Shipment by date_pointer as geoJSON FeatureCollection
-@app.get('/api/v2/nyc/{year}/{month}/{day}/{hour}/{route}/buses/geojson',response_class=PrettyJSONResponse)
-async def fetch_single_Shipment_as_geoJSON(year,month,day,hour,route):
-    date_route_pointer=DateRoutePointer(datetime(year=int(year),
-                                                 month=int(month),
-                                                 day=int(day),
-                                                 hour=int(hour)),
-                                        route.upper())
-    return Shipment(date_route_pointer).to_FeatureCollection()
-
-
-# todo update so that it will work for a less precise date path (e.g. just year/month/day or year/month or year)
-#------------------------------------------------------------------------------------------------------------------------
-# ENDPOINT /api/v2/nyc/{year}/{month}/{day}/{hour}/routes
-# FUNCTION List routes with shipment data for period specified.
-@app.get('/api/v2/nyc/{year}/{month}/{day}/{hour}/routes',response_class=PrettyJSONResponse)
-async def list_all_routes_for_hour(year,month,day,hour):
-    store = make_store() #bug is this expensive/not scalable for each request?
-    date_pointer=DatePointer(datetime(year=int(year),month=int(month),day=int(day),hour=int(hour)))
-    routes = sorted(store.list_routes_in_store(date_pointer))
-    result = {"year":year,
-              "month":month,
-              "day":day,
-              "hour":hour,
-              "routes": routes}
-    return result
+# ENDPOINT /api/v2/nyc/shipments?route=BX4&year=2021&month=6&day=10&hour=4
+# FUNCTION List shipments available for a given set of arguments
+@app.get('/api/v2/shipments',response_class=PrettyJSONResponse)
+async def list_shipments_by_query_all_fields_optional(
+        year: Optional[int] = Query(None, ge=2020, le=2050), #future populate these #s based on DataStore's metadata about what's in the data (e.g. prevent a request for something that isn't there
+        month: Optional[int] = Query(None, ge=1, le=12),
+        day: Optional[int] = Query(None, ge=1, le=31),
+        hour: Optional[int] = Query(None, ge=0, le=23),
+        route: Optional[str] = Query(None, max_length=6)
+):
+    store = make_store()
+    params = {
+        'route':route.upper(),
+        'year':year,
+        'month':month,
+        'day':day,
+        'hour':hour
+    }
+    shipments = store.find_query_shipments(params)
+    return {"query":params,
+            "shipments": shipments}
 
 # todo update so that it will work for a less precise date path (e.g. just year/month/day or year/month or year)
 #------------------------------------------------------------------------------------------------------------------------
 # ENDPOINT /api/v2/nyc/{route}
 # FUNCTION List shipments available for a given route
 @app.get('/api/v2/nyc/{route}',response_class=PrettyJSONResponse)
-async def list_all_shipments_in_history_for_route(route):
+async def list_all_shipments_in_history_for_route(
+        route: str = Query("M15", max_length=6)):
     store = make_store()
     route_shipments = store.find_route_shipments(route.upper())
     shipments = [
@@ -143,31 +110,82 @@ async def list_all_shipments_in_history_for_route(route):
     return {"route":route.upper(),
             "shipments": shipments_sorted}
 
-#todo add validation for the query parameters
+
 #------------------------------------------------------------------------------------------------------------------------
-# ENDPOINT /api/v2/nyc/shipments?route=BX4&year=2021&month=6&day=10&hour=4
-# FUNCTION List shipments available for a given set of arguments
-@app.get('/api/v2/shipments',response_class=PrettyJSONResponse)
-async def list_all_shipments_for_query(
-        year: Optional[int] = None,
-        month: Optional[int] = None,
-        day: Optional[int] = None,
-        hour: Optional[int] = None,
-        route: Optional[str] = None
+# ENDPOINT /api/v2/nyc/buses/{year}/{month}/{day}/{hour}/{route}/json
+# FUNCTION get a single Shipment by date_pointer as JSON
+@app.get('/api/v2/nyc/{year}/{month}/{day}/{hour}/{route}/buses')
+# after https://stackoverflow.com/questions/62455652/how-to-serve-static-files-in-fastapi
+async def fetch_single_shipment(*,
+        year: int = Path(..., ge=2020, le=2050), #future populate these #s based on DataStore's metadata about what's in the data (e.g. prevent a request for something that isn't there
+        month: int = Path(..., ge=1, le=12),
+        day: int = Path(..., ge=1, le=31),
+        hour: int = Path(..., ge=0, le=23),
+        route: str = Path(..., max_length=6)
 ):
-    store = make_store()
-    params = {
-        'route':route.upper(), #bug only the route filter isnt working
-        'year':year,
-        'month':month,
-        'day':day,
-        'hour':hour
-    }
-    shipments = store.find_query_shipments(params)
-    return {"query":params,
-            "shipments": shipments}
+    shipment_to_get = 'data/store/shipments/{}/{}/{}/{}/{}/shipment_{}-{}-{}-{}-{}.json'.\
+        format(year,month,day,hour,route.upper(),year,month,day,hour,route.upper())
+    if not isfile(shipment_to_get):
+        return Response(status_code=404)
+    with open(shipment_to_get) as f:
+        content = f.read()
+    return Response(content, media_type='application/json')
 
 
+#------------------------------------------------------------------------------------------------------------------------
+# ENDPOINT /api/v2/nyc/buses/{year}/{month}/{day}/{hour}/{route}/geojson
+# FUNCTION get a single Shipment by date_pointer as geoJSON FeatureCollection
+@app.get('/api/v2/nyc/{year}/{month}/{day}/{hour}/{route}/buses/geojson',response_class=PrettyJSONResponse)
+async def fetch_single_Shipment_as_geoJSON(
+        *,
+        year: int = Path(..., ge=2020, le=2050), #future populate these #s based on DataStore's metadata about what's in the data (e.g. prevent a request for something that isn't there
+        month: int = Path(..., ge=1, le=12),
+        day: int = Path(..., ge=1, le=31),
+        hour: int = Path(..., ge=0, le=23),
+        route: str = Path(..., max_length=6)
+):
+    date_route_pointer=DateRoutePointer(datetime(year=int(year),
+                                                 month=int(month),
+                                                 day=int(day),
+                                                 hour=int(hour)),
+                                        route.upper())
+    return Shipment(date_route_pointer).to_FeatureCollection()
+
+
+# todo update so that it will work for a less precise date path (e.g. just year/month/day or year/month or year)
+#------------------------------------------------------------------------------------------------------------------------
+# ENDPOINT /api/v2/nyc/{year}/{month}/{day}/{hour}/routes
+# FUNCTION List routes with shipment data for period specified.
+@app.get('/api/v2/nyc/{year}/{month}/{day}/{hour}/routes',response_class=PrettyJSONResponse)
+async def list_all_routes_for_hour(
+        *,
+        year: int = Path(..., ge=2020, le=2050), #future populate these #s based on DataStore's metadata about what's in the data (e.g. prevent a request for something that isn't there
+        month: int = Path(..., ge=1, le=12),
+        day: int = Path(..., ge=1, le=31),
+        hour: int = Path(..., ge=0, le=23)
+):
+    store = make_store() #bug is this expensive/not scalable for each request?
+    date_pointer=DatePointer(datetime(year=int(year),month=int(month),day=int(day),hour=int(hour)))
+    routes = sorted(store.list_routes_in_store(date_pointer))
+    result = {"year":year,
+              "month":month,
+              "day":day,
+              "hour":hour,
+              "routes": routes}
+    return result
+
+#------------------------------------------------------------------------------------------------------------------------
+# ENDPOINT /api/v2/nyc/dashboard
+# FUNCTION Dashboard metadata showing number of barrels and shipments per hour per route currently stored.
+@app.get('/api/v2/nyc/dashboard')
+# after https://stackoverflow.com/questions/62455652/how-to-serve-static-files-in-fastapi
+async def fetch_dashboard_data():
+    filename= 'data/dashboard.csv'
+    if not isfile(filename):
+        return Response(status_code=404)
+    with open(filename) as f:
+        content = f.read()
+        return Response(content, media_type='text/csv')
 
 
 #------------------------------------------------------------------------------------------------------------------------
