55e6d9fb92728f01a9cb2890227f010acd898e27
Anthony Townsend <anthony@starcitygroup.us>
2021-07-19 22:23:35 -0400

    new approach -- process each table to its own datastore

--- archive_db2shipments.py
+++ archive_db2shipments.py
@@ -1,49 +1,19 @@
+# archive_db2shipments.py
+# july 2021
+# dumps shipment files from a collection of v1 nycbuswatcher database tables of identical format to ./tmp
+
+import datetime as dt
+import argparse
+import itertools
 from collections import defaultdict
 from sqlalchemy import (Column, Date, Integer, MetaData, Table, String, create_engine, select, insert)
-import itertools
-
-# database setup
-# define the lookup table
-metadata = MetaData()
-lookup = Table(
-	'buses_lookup', metadata,
-	Column('origin_table', String(length=127), nullable=False, index=True),
-	Column('id', Integer),
-	Column('service_date', Date),
-)
-
-import argparse
-parser = argparse.ArgumentParser(description='NYCbuswatcher shipment dumper, dumps fromv1 database table to shipment json files')
-# parser.add_argument('datadir', type=str, help="Path to dump to.")
-parser.add_argument('-l', '--localhost', action='store_true', help="Run on localhost (otherwise uses docker container hostname 'db')")
-args = parser.parse_args()
-
-if args.localhost == True:
-	engine = create_engine('mysql://nycbuswatcher:bustime@localhost:3306/buses')
-else:
-	engine = create_engine('mysql://nycbuswatcher:bustime@db:3306/buses')
-metadata.create_all(bind=engine)
-
-
-# load the DataStore
 from common.Models import *
 
-#create datalist as list of DatePointer objects by hour
-import datetime as dt
+######################################################
+
+# define datespace
 date1 = '2020-10-16'
 date2 = '2021-05-31'
-datelist = []
-start = dt.datetime.strptime(date1, '%Y-%m-%d')
-end = dt.datetime.strptime(date2, '%Y-%m-%d')
-step = dt.timedelta(days=1)
-while start <= end:
-	for hr in range(24):
-		datelist.append(
-			DatePointer(
-				start+dt.timedelta(hours=hr)
-			)
-		)
-	start += step
 
 # define tablespace
 tablespace=['buses_reprocessed_2020_10',
@@ -62,49 +32,81 @@ tablespace=['buses_reprocessed_2020_10',
 			'buses_reprocessed_dumped_2021_06'
 			]
 
+# define template
 
-# SECOND APPROACH -- iterate over tables with a query for each date
+def make_monitored_vehicle_journey(bus,data):
+	return \
+		{
+			"MonitoredVehicleJourney": {
+				"LineRef": f'{bus.route_long}',
+				"DirectionRef": f'{bus.direction}',
+				"FramedVehicleJourneyRef": {
+					"DataFrameRef": f'{bus.service_date}',
+					"DatedVehicleJourneyRef": f'{bus.trip_id}'
+				},
+				"JourneyPatternRef": f'{bus.gtfs_shape_id}',
+				"PublishedLineName": f'{bus.route_short}',
+				"OperatorRef": f'{bus.agency}',
+				"OriginRef": f'{bus.origin_id}',
+				"DestinationName": f'{bus.destination_name}',
+				"OriginAimedDepartureTime": "2021-07-13T17:57:00.000-04:00",
+				"SituationRef": [], #bug how to parse -- 'alert': ['SituationRef', 'SituationSimpleRef']
+				"VehicleLocation": {
+					"Longitude": f'{bus.lon}',
+					"Latitude": f'{bus.lat}'
+				},
+				"Bearing": f'{bus.bearing}',
+				"ProgressRate": f'{bus.progress_rate}',
+				"ProgressStatus": f'{bus.progress_status}',
+				"BlockRef": f'{bus.gtfs_block_id}',
+				"VehicleRef": f'{bus.vehicle_id}',
+				"MonitoredCall": {
+					"Extensions": {
+						"Capacities": {
+							"EstimatedPassengerCount": f'{bus.passenger_count}',
+							"DistanceFromCall": f'{bus.next_stop_d}',
+							"CallDistanceAlongRoute": f'{bus.next_stop_d_along_route}'
+						}
+					}
+				}
+			},
+			"RecordedAtTime": f'{bus.timestamp.isoformat()}' #bug compare with standard shipment
+		}
 
-# create a datastore in cwd
-store = DataStore(Path.cwd() / 'tmp')
 
-# 2. then iterate over dates from 10-15-2020 to 04-30-2021 through that, pulling records that match from the right tables
-# make a shipment for each
-# concatenate them into a shipment file and dump it
+######################################################
 
-for date in datelist:
+def get_datelist(date1,date2):
+	datelist = []
+	start = dt.datetime.strptime(date1, '%Y-%m-%d')
+	end = dt.datetime.strptime(date2, '%Y-%m-%d')
+	step = dt.timedelta(days=1)
+	while start <= end:
+		for hr in range(24):
+			datelist.append(
+				DatePointer(
+					start+dt.timedelta(hours=hr)
+				)
+			)
+		start += step
+	return datelist
 
-	date_str=date.timestamp.strftime('%Y-%m-%d')
-
-	# todo might be more consistent to have both conditions query against timestamp
-
-	query = f"""
-	SELECT * FROM buses_reprocessed_2020_10 UNION
-	SELECT * FROM buses_reprocessed_2020_11 UNION
-	SELECT * FROM buses_reprocessed_2020_12_a UNION
-	SELECT * FROM buses_reprocessed_2020_12_b UNION
-	SELECT * FROM buses_reprocessed_2021_01_a UNION
-	SELECT * FROM buses_reprocessed_2021_01_b UNION
-	SELECT * FROM buses_reprocessed_2021_02_a UNION
-	SELECT * FROM buses_reprocessed_2021_02_b UNION
-	SELECT * FROM buses_reprocessed_2021_03_a UNION
-	SELECT * FROM buses_reprocessed_2021_03_b UNION
-	SELECT * FROM buses_reprocessed_2021_04_a UNION
-	SELECT * FROM buses_reprocessed_2021_04_b UNION
-	SELECT * FROM buses_reprocessed_dumped_2021_05 UNION
-	SELECT * FROM buses_reprocessed_dumped_2021_06
-	WHERE service_date='{date_str}' 
-	AND HOUR(timestamp) = {date.hour}
-	"""
-
+def get_hour_of_data(table, datehour):
 
 	with engine.connect() as conn:
+		hour_of_data = []
+		date_str=datehour.timestamp.strftime('%Y-%m-%d')
+		# todo might be more consistent to have both conditions query against timestamp
+		query = f"""SELECT * FROM {table} WHERE service_date='{date_str}' AND HOUR(timestamp) = {datehour.hour}"""
+		print(f'querying: table {table} \t\t date {date_str} \t hour {datehour.hour} \t completed at {dt.datetime.now()}')
 		results = conn.execute(query)
-		print(f'queried: {date_str}, hour {date.hour} completed at {dt.datetime.now()}')
-		rows = [x for x in results]
+		return [x for x in results]
+
+
+def dump_hour(store, hour_of_data):
 
 		# iterate over the groups
-		for route_long, route_group in itertools.groupby(rows, lambda x: x.route_long):
+		for route_long, route_group in itertools.groupby(hour_of_data, lambda x: x.route_long):
 
 			data = defaultdict()
 			data['VehicleActivity'] = []
@@ -112,57 +114,53 @@ for date in datelist:
 			for bus in route_group:
 				# process the route_group into a feed and feed it to store.make_barrels
 
-				monitored_vehicle_journey = \
-					{
-						"MonitoredVehicleJourney": {
-							"LineRef": f'{bus.route_long}',
-							"DirectionRef": f'{bus.direction}',
-							"FramedVehicleJourneyRef": {
-								"DataFrameRef": f'{bus.service_date}',
-								"DatedVehicleJourneyRef": f'{bus.trip_id}'
-							},
-							"JourneyPatternRef": f'{bus.gtfs_shape_id}',
-							"PublishedLineName": f'{bus.route_short}',
-							"OperatorRef": f'{bus.agency}',
-							"OriginRef": f'{bus.origin_id}',
-							"DestinationName": f'{bus.destination_name}',
-							"OriginAimedDepartureTime": "2021-07-13T17:57:00.000-04:00",
-							"SituationRef": [], #bug how to parse -- 'alert': ['SituationRef', 'SituationSimpleRef']
-							"VehicleLocation": {
-								"Longitude": f'{bus.lon}',
-								"Latitude": f'{bus.lat}'
-							},
-							"Bearing": f'{bus.bearing}',
-							"ProgressRate": f'{bus.progress_rate}',
-							"ProgressStatus": f'{bus.progress_status}',
-							"BlockRef": f'{bus.gtfs_block_id}',
-							"VehicleRef": f'{bus.vehicle_id}',
-							"MonitoredCall": {
-								"Extensions": {
-									"Capacities": {
-										"EstimatedPassengerCount": f'{bus.passenger_count}',
-										"DistanceFromCall": f'{bus.next_stop_d}',
-										"CallDistanceAlongRoute": f'{bus.next_stop_d_along_route}'
-									}
-								}
-							}
-						},
-						"RecordedAtTime": f'{bus.timestamp.isoformat()}' #bug compare with standard shipment
+
+				data['VehicleActivity'].append(make_monitored_vehicle_journey(bus,data))
+
+				fake_response =  {"Siri" :{
+					"ServiceDelivery" : {
+						"VehicleMonitoringDelivery" : []
 					}
-
-				data['VehicleActivity'].append(monitored_vehicle_journey)
-
-			fake_response =  {"Siri" :{
-								"ServiceDelivery" : {
-									"VehicleMonitoringDelivery" : []
-													}
-									}
-							}
+				}
+			}
 
 
-			fake_response['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'].append(data)
+				fake_response['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'].append(data)
 
-			feeds = [{ route_long: fake_response} ]
-			store.make_barrels(feeds, date)
+				feeds = [{ route_long: fake_response} ]
+				store.make_barrels(feeds, datehour)
 
-	store.render_barrels()
+		store.render_barrels()
+		return
+
+
+if __name__=="__main__":
+
+	# parse arguments
+	parser = argparse.ArgumentParser(description='NYCbuswatcher shipment dumper, dumps fromv1 database table to shipment json files')
+	parser.add_argument('-l', '--localhost', action='store_true', help="Run on localhost (otherwise uses docker container hostname 'db')")
+	args = parser.parse_args()
+
+	# setup the db connection
+	if args.localhost == True:
+		engine = create_engine('mysql://nycbuswatcher:bustime@localhost:3306/buses')
+	else:
+		engine = create_engine('mysql://nycbuswatcher:bustime@db:3306/buses')
+
+	#create datelist as list of DatePointer objects by hour
+
+
+	# todo method 3 create a separate datastore for each table
+	# todo figure out how to combine -- then identify, load, and combine any that overlap
+
+
+	for table in tablespace:
+
+		# create a separate datastore for each table
+		store = DataStore(Path.cwd() / 'tmp' / f"""{table}""")
+
+		#get each hour from the table and render it
+		for datehour in get_datelist(date1,date2):
+			hour_of_data = 	get_hour_of_data(table, datehour)
+			dump_hour(store, hour_of_data)
+
