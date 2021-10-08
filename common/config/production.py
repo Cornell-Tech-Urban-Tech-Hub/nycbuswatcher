ba24f2b9fa31857eb2dbd61007bbca8487c48077
Anthony Townsend <anthony@starcitygroup.us>
2021-10-08 07:00:01 -0400

    updated urls for https

--- common/config/production.py
+++ common/config/production.py
@@ -1,7 +1,7 @@
 config = {
-    'base_api_url' : 'http://api.buswatcher.org:80/api/v2/nyc',
-    'shipment_api_url' : 'http://api.buswatcher.org:80/api/v2/nyc/{}/{}/{}/{}/{}/buses',# hostname and port inside the stack
-    'glacier_api_url' : 'http://api.buswatcher.org:80/api/v2/nyc/{}/{}/{}/{}/{}/archive',
-    'history_api_url' : 'http://api.buswatcher.org:80/api/v2/nyc/{}/history',
+    'base_api_url' : 'https://api.buswatcher.org:80/api/v2/nyc',
+    'shipment_api_url' : 'https://api.buswatcher.org:80/api/v2/nyc/{}/{}/{}/{}/{}/buses',# hostname and port inside the stack
+    'glacier_api_url' : 'https://api.buswatcher.org:80/api/v2/nyc/{}/{}/{}/{}/{}/archive',
+    'history_api_url' : 'https://api.buswatcher.org:80/api/v2/nyc/{}/history',
     'http_connections': 20
 }
